7dcf4001b44f2f0b60a5d4e9aad3b0c830eb52f4
Andre Backman <abackman@student.hive.fi>
2022-06-10 15:22:19 +0300

    updated header file

--- includes/push_swap.h
+++ includes/push_swap.h
@@ -6,7 +6,7 @@
 /*   By: abackman <abackman@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/06/07 17:10:26 by abackman          #+#    #+#             */
-/*   Updated: 2022/06/07 17:14:53 by abackman         ###   ########.fr       */
+/*   Updated: 2022/06/10 15:19:28 by abackman         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -14,5 +14,29 @@
 # define PUSH_SWAP_H
 # include "../libft/libft.h"
 # include "../../ft_printf/includes/ft_printf.h"
+
+typedef enum e_moves
+{
+	SA = 1,
+	SB,
+	SS,
+	PA,
+	PB,
+	RA,
+	RB,
+	RR,
+	RRA,
+	RRB,
+	RRR,
+}	t_moves;
+
+typedef struct s_tack;
+{
+	int	*a_stack;
+	int	*b_stack;
+	int	min;
+	int	max;
+	int	elem;
+}	t_stack;
 
 #endif
