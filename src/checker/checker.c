79098b8edf18b87024d6d7580d217cfc77537538
Andre Backman <abackman@student.hive.fi>
2022-06-30 12:39:06 +0300

    removed init_moves function, ft_strsplit does the job in main function

--- src/checker/checker.c
+++ src/checker/checker.c
@@ -6,7 +6,7 @@
 /*   By: abackman <abackman@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/05/31 18:07:57 by abackman          #+#    #+#             */
-/*   Updated: 2022/06/13 16:05:53 by abackman         ###   ########.fr       */
+/*   Updated: 2022/06/30 12:38:16 by abackman         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -31,62 +31,20 @@ static int	return_status(int status)
 	}
 }
 
-static char	**init_moves(void)
-{
-	char	**str;
-
-	str = (char **)malloc(12 * sizeof(char *));
-	if (!str)
-		return (NULL);
-	str[0] = ft_strdup("sa");
-	str[1] = ft_strdup("sb");
-	str[2] = ft_strdup("ss");
-	str[3] = ft_strdup("pa");
-	str[4] = ft_strdup("pb");
-	str[5] = ft_strdup("ra");
-	str[6] = ft_strdup("rb");
-	str[7] = ft_strdup("rr");
-	str[8] = ft_strdup("rra");
-	str[9] = ft_strdup("rrb");
-	str[10] = ft_strdup("rrr");
-	str[11] = NULL;
-	return (str);
-}
-
-static int	check_input(char *input, char **valid)
-{
-	int	count;
-
-	count = 0;
-	while (valid[count] != NULL)
-	{
-		if (!ft_strcmp(input, valid[count]))
-			return (1);
-		count++;
-	}
-	return (0);
-}
-
 int	main(int ac, char **av)
 {
-	t_stack	*stack;
+	t_pusha	*stacks;
 	char	**moves;
-	char	buf[10];
-	int	count;
-	
+
 	if (ac < 2)
 		return (1);
-	moves = init_moves();
-	while (1)
-	{
-		read(STDIN_FILENO, &buf, 10);
-		if (!check_input(*av, moves))
-			return (return_status(-1));
-		else
-			make_move(*av, &stack);
-	}
-	if (sorted(stack))
-		return (return_status(0));
-	else
-		return (return_status(1));
+	stacks = (t_pusha *)malloc(sizeof(t_pusha));
+	if (!stacks)
+		return(return_status(-1));
+	moves = ft_strsplit("sa sb ss pa pb ra rb rr rra rrb rrr", (char)32);
+	if (moves == NULL)
+		return(return_status(-1));
+	if (!check_input(&stacks, ac, av))
+		return (return_status(-1));
+	
 }
