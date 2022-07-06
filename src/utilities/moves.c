e95e929df8e5e774d1658feec15dbc3e14dc4ba2
Andre Backman <abackman@student.hive.fi>
2022-07-06 18:48:47 +0300

    moved out of place functions to stacks.c

--- src/utilities/moves.c
+++ src/utilities/moves.c
@@ -6,48 +6,22 @@
 /*   By: abackman <abackman@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/06/10 15:43:00 by abackman          #+#    #+#             */
-/*   Updated: 2022/06/13 16:35:27 by abackman         ###   ########.fr       */
+/*   Updated: 2022/07/06 18:34:10 by abackman         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "../../includes/push_swap.h"
-t_stack	*free_stack(t_stack *stack)
-{
-	free(stack->a_stack);
-	free(stack->b_stack);
-	free(stack);
-	return (NULL);
-}
 
-static int	validate_input(t_stack **stack, char *input)
+/* int	valid_move(char *str, char **valid_moves)
 {
-	int			i;
-	long long	tmp;
+	int	i;
 
 	i = 0;
-	while (input[i])
+	while (valid_moves[i] != NULL)
 	{
-		tmp = ft_atoi()
+		if (ft_strcmp((const char)str, (const char)valid_moves) == 0)
+			return (i);
+		i++;
 	}
-}
-
-t_stack	*check_input(int ac, char **input)
-{
-	int		ret;
-	int		count;
-	t_stack	*stack;
-
-	ret = 0;
-	count = 2;
-	stack = (t_stack *)malloc(sizeof(t_stack));
-	if (!stack)
-		return (NULL);
-	while (count <= ac)
-	{
-		ret = validate_input(&stack, input[count - 1]);
-		if (!ret)
-			break ;
-		count++;
-	}
-	return (stack);
-}
+	return (-1);
+} */
