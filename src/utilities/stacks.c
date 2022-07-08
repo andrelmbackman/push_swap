e76dcf0d0a75c782ecbcdd3bdaaefbf4113d32f6
Andre Backman <abackman@student.hive.fi>
2022-07-08 15:53:23 +0300

    made add stacks function traverse the list, giving it the head

--- src/utilities/stacks.c
+++ src/utilities/stacks.c
@@ -6,7 +6,7 @@
 /*   By: abackman <abackman@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/07/06 15:21:02 by abackman          #+#    #+#             */
-/*   Updated: 2022/07/07 16:54:30 by abackman         ###   ########.fr       */
+/*   Updated: 2022/07/08 15:48:57 by abackman         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -32,20 +32,31 @@ int	free_stacks(t_pusha *stacks, int status)
 	}
 	free(stacks);
 	stacks = NULL;
+	//system("leaks checker");
 	return (return_status(status));
 }
 
-t_stack	*add_stack(int value)
+int	add_stack(t_stack **head, int value)
 {
 	t_stack	*new;
+	t_stack	*last;
 
 	//ft_printf("\nadd_stack\n");
+	last = *head;
 	new = (t_stack *)malloc(sizeof(t_stack));
 	if (!new)
-		return (NULL);
+		return (0);
 	new->num = value;
 	new->next = NULL;
-	return (new);
+	if (*head == NULL)
+	{
+		*head = new;
+		return (1);
+	}
+	while (last->next != NULL)
+		last = last->next;
+	last->next = new;
+	return (1);
 }
 
 int	check_stacks(t_pusha *stacks)
@@ -55,7 +66,7 @@ int	check_stacks(t_pusha *stacks)
 	head = stacks->a_stack;
 	if (stacks->b_size != 0)
 		return (free_stacks(stacks, KO));
-//	ft_printf("Checking stack a: %d\n", stacks->a_stack->num);
+	ft_printf("Checking stack a: %d\n", stacks->a_stack->num);
 	while (stacks->a_stack->next != NULL)
 	{
 		if (stacks->a_stack->next->num < stacks->a_stack->num)
