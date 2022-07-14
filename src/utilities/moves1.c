ffdf70765d498e810f626f1c8f489b32a18f1e41
Andre Backman <abackman@student.hive.fi>
2022-07-14 15:18:21 +0300

    renamed moves.c to moves1.c

--- src/utilities/moves1.c
+++ src/utilities/moves1.c
@@ -6,22 +6,37 @@
 /*   By: abackman <abackman@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/06/10 15:43:00 by abackman          #+#    #+#             */
-/*   Updated: 2022/07/06 18:34:10 by abackman         ###   ########.fr       */
+/*   Updated: 2022/07/14 15:16:26 by abackman         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "../../includes/push_swap.h"
 
-/* int	valid_move(char *str, char **valid_moves)
+int	exec_sa(t_pusha *stacks)
 {
-	int	i;
+	return (1);
+}
 
-	i = 0;
-	while (valid_moves[i] != NULL)
-	{
-		if (ft_strcmp((const char)str, (const char)valid_moves) == 0)
-			return (i);
-		i++;
-	}
-	return (-1);
-} */
+int	exec_sb(t_pusha *stacks)
+{
+	return (1);
+}
+
+int	exec_ss(t_pusha *stacks)
+{
+	if (!exec_sa(stacks))
+		return (0);
+	if (!exec_sb(stacks))
+		return (0);
+	return (1);
+}
+
+int	exec_pa(t_pusha *stacks)
+{
+	return (1);
+}
+
+int	exec_pb(t_pusha *stacks)
+{
+	return (1);
+}
