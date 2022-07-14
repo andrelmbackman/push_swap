76cc099e426faa7cec15463a4d61c0a80827449f
Andre Backman <abackman@student.hive.fi>
2022-07-14 15:26:34 +0300

    created function dispatch table

--- includes/push_swap.h
+++ includes/push_swap.h
@@ -6,7 +6,7 @@
 /*   By: abackman <abackman@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/06/07 17:10:26 by abackman          #+#    #+#             */
-/*   Updated: 2022/07/08 15:44:24 by abackman         ###   ########.fr       */
+/*   Updated: 2022/07/14 15:26:15 by abackman         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -17,6 +17,21 @@
 # define ERROR -1
 # define KO 0
 # define OK 1
+
+static const t_able	g_pushswap[12] = {
+	exec_sa,
+	exec_sb,
+	exec_ss,
+	exec_pa,
+	exec_pb,
+	exec_ra,
+	exec_rb,
+	exec_rr,
+	exec_rra,
+	exec_rrb,
+	exec_rrr,
+	NULL
+};
 
 typedef enum e_moves
 {
@@ -49,9 +64,29 @@ typedef struct s_pusha
 	int		b_size;
 }	t_pusha;
 
+/*
+** Functions for executing the moves
+*/
+int	exec_sa(t_pusha *stacks);
+int	exec_sb(t_pusha *stacks);
+int	exec_ss(t_pusha *stacks);
+int	exec_pa(t_pusha *stacks);
+int	exec_pb(t_pusha *stacks);
+int	exec_ra(t_pusha *stacks);
+int	exec_rb(t_pusha *stacks);
+int	exec_rr(t_pusha *stacks);
+int	exec_rra(t_pusha *stacks);
+int	exec_rrb(t_pusha *stacks);
+int	exec_rrr(t_pusha *stacks);
+
+/*
+** Other functions
+*/
+
 int		return_status(int status);
 int		check_stacks(t_pusha *stacks);
 int		free_stacks(t_pusha *stacks, int status);
+int		moves_check(char **valid_moves, t_pusha *stacks);
 int		check_ints(t_pusha *stacks, int ac, char **input);
 int		add_stack(t_stack **head, int value);
 t_pusha	*init_stacks(void);
