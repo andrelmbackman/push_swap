dc418c77ee04def046a1a723a5d278808250c478
Andre Backman <abackman@student.hive.fi>
2022-07-14 15:26:59 +0300

    completed exec_move function

--- src/checker/moves_check.c
+++ src/checker/moves_check.c
@@ -1,27 +1,32 @@
 /* ************************************************************************** */
 /*                                                                            */
 /*                                                        :::      ::::::::   */
-/*   check_input.c                                      :+:      :+:    :+:   */
+/*   moves_check.c                                      :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
 /*   By: abackman <abackman@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/07/06 15:00:18 by abackman          #+#    #+#             */
-/*   Updated: 2022/07/08 15:49:51 by abackman         ###   ########.fr       */
+/*   Updated: 2022/07/14 15:26:03 by abackman         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "../../includes/push_swap.h"
+
+static int	exec_move(t_pusha *stacks, int status)
+{
+	if (stacks == NULL || status == -1)
+		return (-1);
+	return (g_pushswap[status](stacks));
+}
 
 static int	valid_move(char *buf, char **valid)
 {
 	int	i;
 
 	i = 0;
-	if (!get_next_line((const int)STDIN_FILENO, &buf))
-		return (-1);
 	while (valid[i] != NULL)
 	{
-		if (!ft_strcmp((const char)valid[i], (const char)buf));
+		if (!ft_strcmp((const char *)valid[i], (const char *)buf))
 			return (i);
 		i++;
 	}
@@ -30,7 +35,30 @@ static int	valid_move(char *buf, char **valid)
 
 int	moves_check(char **valid_moves, t_pusha *stacks)
 {
-	int	status;
+	int		status;
+	char	*buf;
+
+	status = 0;
+	while (1)
+	{
+		if (!get_next_line((const int)STDIN_FILENO, &buf))
+			break ;
+		status = valid_move(buf, valid_moves);
+		ft_strdel(&buf);
+		if (status == -1)
+		{
+			ft_free_arr(valid_moves, (size_t)10);
+			return (free_stacks(stacks, -1));
+		}
+		else
+			exec_move(stacks, status);
+	}
+	return (check_stacks(stacks));	
+}
+
+/* int	moves_check(char **valid_moves, t_pusha *stacks)
+{
+	int		status;
 	char	*buf;
 
 	status = 0;
@@ -45,8 +73,8 @@ int	moves_check(char **valid_moves, t_pusha *stacks)
 			return (free_stacks(stacks, -1));
 		}
 		else
-			execute_move(stacks, status);
+			exec_move(stacks, status);
 	}
 	return (check_stacks(stacks));	
 }
-
+ */
