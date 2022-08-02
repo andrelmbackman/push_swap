18c603d07e7c9860667b12588ec373c652579920
Andre Backman <abackman@student.hive.fi>
2022-08-02 19:30:19 +0300

    started helper functions for the moves functions

---
+++ src/utilities/moves_helpers.c
@@ -0,0 +1,28 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   moves_helpers.c                                    :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: abackman <abackman@student.42.fr>          +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2022/08/02 19:14:55 by abackman          #+#    #+#             */
+/*   Updated: 2022/08/02 19:26:31 by abackman         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "../../includes/push_swap.h"
+
+void	add_head(t_stack *stack, t_stack *new)
+{
+	
+}
+
+void	rm_head(t_stack *stack)
+{
+	
+}
+
+void	add_last(t_stack *stack, t_stack *new)
+{
+	
+}
