e24407665f336e7677e7e00b90693b8926be39e2
Andre Backman <abackman@student.hive.fi>
2022-08-02 19:29:05 +0300

    updated moves functions

--- src/utilities/moves1.c
+++ src/utilities/moves1.c
@@ -6,7 +6,7 @@
 /*   By: abackman <abackman@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/06/10 15:43:00 by abackman          #+#    #+#             */
-/*   Updated: 2022/08/01 14:23:25 by abackman         ###   ########.fr       */
+/*   Updated: 2022/08/02 19:02:54 by abackman         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -83,6 +83,43 @@ int	exec_pb(t_pusha *stacks)
 
 	if ((stacks->a_stack == NULL) || (stacks->a_size = 0))
 		return (0);
+	if ((stacks->b_stack == NULL) || (stacks->b_size = 0))
+		first_stack(&stacks->b_stack, stacks->a_stack);
+	else
+	{
+		//ft_printf("\nPB: b_stack not empty\n");
+		tmp = stacks->a_stack;
+		stacks->b_stack->prev->next = tmp;
+		tmp->prev = stacks->b_stack->prev;
+		stacks->b_stack->prev = tmp;
+		tmp->next = stacks->b_stack;
+		stacks->b_stack->top = 0;
+		stacks->b_stack = tmp;
+	}
+	stacks->a_size--;
+	head = stacks->a_stack;
+	stacks->a_stack = stacks->a_stack->next;
+	stacks->a_stack->prev = head->prev;
+	stacks->a_stack->prev->next = stacks->a_stack;
+	stacks->a_stack->top = 1;
+	ft_printf("\nchecking whether pb works..\na:%d b:%d\n\n", \
+	stacks->a_stack->next->next->num, stacks->b_stack->num);
+	return (1);
+}
+
+/* int	exec_pb(t_pusha *stacks)
+{
+	t_stack	*tmp;
+	t_stack	*head;
+
+	if ((stacks->a_stack == NULL) || (stacks->a_size = 0))
+		return (0);
+	if ((stacks->b_stack == NULL) || (stacks->b_size = 0))
+		first_stack(&stacks->b_stack, stacks->a_stack);
+	else
+	{
+		
+	}
 	tmp = stacks->a_stack;
 	head = stacks->a_stack->next;
 	head->top = 1;
@@ -96,4 +133,4 @@ int	exec_pb(t_pusha *stacks)
 	tmp->next = stacks->b_stack;
 	stacks->b_stack = tmp;
 	return (1);
-}
+} */
