8227a9ea042df89be30ae69290ead8f8764d3453
Andre home <andrebackmann@gmail.com>
2022-08-09 13:00:43 +0300

    removed extra check

--- libft/ft_itoa.c
+++ libft/ft_itoa.c
@@ -3,10 +3,10 @@
 /*                                                        :::      ::::::::   */
 /*   ft_itoa.c                                          :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
-/*   By: abackman <abackman@student.42.fr>          +#+  +:+       +#+        */
+/*   By: abackman <abackman@student.hive.fi>        +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/11/19 14:19:59 by abackman          #+#    #+#             */
-/*   Updated: 2021/11/29 13:03:16 by abackman         ###   ########.fr       */
+/*   Updated: 2022/08/09 12:57:50 by abackman         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -15,7 +15,7 @@
 static char	*ft_posnum(long num, size_t len, char *str)
 {
 	str[len--] = '\0';
-	while (len >= 0 && num > 0)
+	while (num > 0)
 	{
 		str[len] = ((num % 10) + '0');
 		num /= 10;
