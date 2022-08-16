8a48212cdac883dfc5224d1cd71d4c60e6132479
André Backman <88145164+andrelmbackman@users.noreply.github.com>
2022-08-16 12:35:30 +0300

    Update sort_small.c

--- src/push_swap/sort_small.c
+++ src/push_swap/sort_small.c
@@ -16,11 +16,50 @@ static int	sort_three(t_pusha *stacks)
 {
 	t_stack	*a;
 
+	static int	sort_three(t_pusha *stacks)
+{
+	t_stack	*a;
+
 	a = stacks->a_stack;
 	if (a->num > a->next->num && a->next->num < a->prev->num && a->prev->num\
+	> a->num)
+		return (exec_sa(stacks));
+	else if (a->num > a->next->num && a->next->num < a->prev->num && a->prev->num\
 	< a->num)
+		return (exec_ra(stacks));
+	else if (a->num < a->next->num && a->next->num > a->prev->num && a->prev->num\
+	< a->num)
+		return (exec_rra(stacks));
+	else if (a->num < a->next->num && a->next->num > a->prev->num && a->prev->num\
+	> a->num)
+	{
+		exec_sa(stacks);
+		return (exec_ra(stacks));
+	}
+	else if (a->num > a->next->num && a->next->num > a->prev->num && a->prev->num\
+	< a->num)
+	{
+		exec_sa(stacks);
+		return (exec_rra(stacks));
+	}
+	return (0);
+}
+
+static int	sort_two(t_pusha *stacks)
+{
+	if (stacks->a_stack->num > stacks->a_stack->next->num)
 		return (exec_sa(stacks));
 	return (0);
+}
+
+int	sort_small(t_pusha *stacks)
+{
+	if (stacks->a_size == 2)
+		return (sort_two(stacks));
+	if (stacks->a_size <= 3)
+		return (sort_three(stacks));
+	return (0);
+}
 }
 
 static int	sort_two(t_pusha *stacks)
