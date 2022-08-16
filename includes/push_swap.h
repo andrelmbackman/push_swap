def92bd5bdc58ab90b9f5a550b6e08a92a30be5e
Andre Backman <abackman@student.hive.fi>
2022-08-16 12:48:52 +0300

    added functions to header

--- includes/push_swap.h
+++ includes/push_swap.h
@@ -3,10 +3,10 @@
 /*                                                        :::      ::::::::   */
 /*   push_swap.h                                        :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
-/*   By: abackman <abackman@student.hive.fi>        +#+  +:+       +#+        */
+/*   By: abackman <abackman@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/06/07 17:10:26 by abackman          #+#    #+#             */
-/*   Updated: 2022/08/09 17:29:10 by abackman         ###   ########.fr       */
+/*   Updated: 2022/08/11 13:32:08 by abackman         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -102,6 +102,7 @@ int		first_stack(t_stack **head, t_stack *new);
 int		sort_stacks(t_pusha *stacks);
 int		sort_small(t_pusha *stacks);
 int		sorted(t_pusha *stacks);
+int		radix_sort(t_pusha *stacks);
 void	add_head(t_stack *stack, t_stack *new);
 void	rm_head(t_stack *stack);
 void	add_to_empty(t_stack *stack, t_stack *new);
