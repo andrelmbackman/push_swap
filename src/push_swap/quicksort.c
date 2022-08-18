2f174ae0f815eb57d79b370a4c9cf8c97b8f5a94
Andre home <andrebackmann@gmail.com>
2022-08-18 18:30:03 +0300

    quicksort inf loops

--- src/push_swap/quicksort.c
+++ src/push_swap/quicksort.c
@@ -3,16 +3,16 @@
 /*                                                        :::      ::::::::   */
 /*   quicksort.c                                        :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
-/*   By: abackman <abackman@student.42.fr>          +#+  +:+       +#+        */
+/*   By: abackman <abackman@student.hive.fi>        +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/08/18 13:42:46 by abackman          #+#    #+#             */
-/*   Updated: 2022/08/18 14:11:17 by abackman         ###   ########.fr       */
+/*   Updated: 2022/08/18 18:28:21 by abackman         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "../../includes/push_swap.h"
 
-static int	check_ss(t_stack *a, t_stack *b)
+/* static int	check_ss(t_stack *a, t_stack *b)
 {
 	if (!a || !b)
 		return (0);
@@ -31,12 +31,12 @@ static int	check_rrr(t_stack *a, t_stack *b)
 		if (b->num > b->prev->num || b->prev->num < b->prev->prev->num)
 			return (1);
 	}
-	return (0);	
-}
+	return (0);
+} */
 
 static int	check_rr(t_stack *a, t_stack *b, int pivot)
 {
-	if (!a || !b)
+	if (a == 0 || b == 0)
 		return (0);
 	if (a->num > pivot)
 	{
@@ -48,25 +48,30 @@ static int	check_rr(t_stack *a, t_stack *b, int pivot)
 
 int	quicksort(t_pusha *stacks)
 {
-	int	pivot;
+	int		pivot;
+	t_stack	*tmp;
 
 	pivot = 0;
+	tmp = stacks->a_stack->prev;
 	while (!sorted(stacks))
 	{
-		pivot = stacks->a_stack->prev->num;
-		while(stacks->a_stack->num != pivot)
+		tmp = stacks->a_stack->prev;
+		pivot = tmp->num;
+		while(stacks->a_stack != tmp)
 		{
 			if (check_rr(stacks->a_stack, stacks->b_stack, pivot))
 				exec_rr(stacks);
-			else if (stacks->a_stack->num < pivot)
+			else if (stacks->a_stack->num > pivot)
 				exec_pb(stacks);
 			else
 				exec_ra(stacks);
+			//ft_printf("\n\npivot: %d a->num: %d tmp->num: %d\n\n", pivot, stacks->a_stack->num, tmp->num);
 		}
-		if (check_rrr(stacks->a_stack, stacks->b_stack))
+		//ft_printf("\nDO YOU EVEN BREAK?\nnum: %d, pivot: %d\n", stacks->a_stack->num, pivot);
+		/* if (check_rrr(stacks->a_stack, stacks->b_stack))
 			exec_rrr(stacks);
 		else if (check_ss(stacks->a_stack, stacks->b_stack))
-			exec_ss(stacks);
+			exec_ss(stacks); */
 		while (stacks->b_stack)
 			exec_pa(stacks);
 	}
