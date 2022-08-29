0fea374c3919f24888c1fd192985529b1895a92e
Andre Backman <abackman@student.hive.fi>
2022-08-29 14:13:04 +0300

    sorts size 100 with 1000 moves

--- src/push_swap/push_chunks1.c
+++ src/push_swap/push_chunks1.c
@@ -6,7 +6,7 @@
 /*   By: abackman <abackman@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/08/25 15:05:05 by abackman          #+#    #+#             */
-/*   Updated: 2022/08/26 19:26:01 by abackman         ###   ########.fr       */
+/*   Updated: 2022/08/29 14:06:54 by abackman         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -22,11 +22,14 @@ static int	find_rotate(t_pusha *stacks, int min, int max)
 	while (front->next != back && back->prev != front)
 	{
 		if ((front->num >= min && front->num <= max) \
-		|| (back->num >= min && back->num <= max) || (front == back))
+		|| (back->num >= min && back->num <= max))
 			break ;
 		front = front->next;
 		back = back->prev;
+		if (front == back)
+			break ;
 	}
+	//ft_printf("\n{red}front: %d back: %d{eoc}\n", front->num, back->num);
 	if (front->num >= min && front->num <= max)
 	{
 		if (back->num >= min && back->num <= max && back->num < front->num)
@@ -42,50 +45,51 @@ static int	find_rotate(t_pusha *stacks, int min, int max)
  ******************** Beware of overrun, like the desired one is still in b?
  */
 
-static void	find_minmax(t_stack *stack, int min, int max, int size)
+static void	find_minmax(t_stack *stack, int *min, int *max, int size)
 {
 	t_stack	*tmp;
 	t_stack	*save;
 
 	tmp = stack;
 	save = NULL;
-	min = 2147483647;
-	ft_printf("\nfind_minmax: \n");
+	*min = 2147483647;
+	//ft_printf("\nfind_minmax: \n");
 	while (tmp->next != stack)
 	{
-		if (tmp->num <= min)
+		if (tmp->num <= *min)
 		{
-			min = tmp->num;
+			*min = tmp->num;
 			save = tmp;
 		}
 		tmp = tmp->next;
 	}
-	if (tmp->num <= min)
+	if (tmp->num <= *min)
 	{
-		min = tmp->num;
+		*min = tmp->num;
 		save = tmp;
 	}
-	max = 0;
-	while (++max <= size)
+	*max = 0;
+	while (++*max < size)
 		save = save->dst_next;
-	max = save->num;
+	*max = save->num;
 }
 
 static void	one_chunk(t_pusha *stacks, int size)
 {
-	int	i;
+	//int	i;
 	int	min;
 	int	max;
 
-	i = 0;
+	//i = 0;
 	min = stacks->min;
 	max = stacks->max;
-	while (++i <= size)
+	while (size > 0)
 	{
-		find_minmax(stacks->a_stack, min, max, size);
-		ft_printf("\nin one_chunk\n");
+		find_minmax(stacks->a_stack, &min, &max, size);
+		//ft_printf("\nin one_chunk - min: %d max: %d size: %d\n", min, max, size);
 		find_rotate(stacks, min, max);
 		exec_pb(stacks);
+		size--;
 	}
 }
 
@@ -103,12 +107,14 @@ int	push_chunks(t_pusha *stacks, int count)
 	size = stacks->a_size / count;
 	while (++i <= count)
 	{
-		print_stacks(stacks);
+		//print_stacks(stacks);
 		//ft_printf("\nin push_chunks\n");
 		if (i == count)
 			size = stacks->a_size;
 		one_chunk(stacks, size);
 	}
-	print_stacks(stacks);
+	//print_stacks(stacks);
+	rotate_pushback(stacks);
+	//print_stacks(stacks);
 	return (0);
 }
--- src/push_swap/push_chunks2.c
+++ src/push_swap/push_chunks2.c
@@ -6,15 +6,66 @@
 /*   By: abackman <abackman@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/08/25 16:20:05 by abackman          #+#    #+#             */
-/*   Updated: 2022/08/26 19:20:51 by abackman         ###   ########.fr       */
+/*   Updated: 2022/08/29 14:06:09 by abackman         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "../../includes/push_swap.h"
 
+static int	find_biggest(t_stack *b)
+{
+	t_stack	*tmp;
+	int		max;
+
+	max = -2147483648;
+	tmp = b;
+	while (tmp->next != b)
+	{
+		if (tmp->num >= max)
+			max = tmp->num;
+		tmp = tmp->next;
+	}
+	if (tmp->num >= max)
+			max = tmp->num;
+	//ft_printf("\nmax: %d\n", max);
+	return (max);
+}
+
+int	rotate_pushback(t_pusha *stacks)
+{
+	t_stack	*tmp;
+	int		i;
+	int		max;
+
+	while (stacks->b_stack != NULL && stacks->b_size)
+	{
+		tmp = stacks->b_stack;
+		i = 0;
+		max = find_biggest(stacks->b_stack);
+		while (tmp->num != max)
+		{
+			i++;
+			tmp = tmp->next;
+		}
+		//ft_printf("\ntmp->num: %d\n", tmp->num);
+		if (i > (stacks->b_size / 2))
+		{
+			while (stacks->b_stack->num != max)
+				exec_rrb(stacks);
+		}
+		else
+		{
+			while (stacks->b_stack->num != max)
+				exec_rb(stacks);
+		}
+		exec_pa(stacks);
+	}
+	return (1);
+}
+
 int	rotate_before_push(t_pusha *stacks, int goal, int direction)
 {
-	ft_printf("\nrotate_before_push goal: %d, dir: %d\n", goal, direction);
+	//ft_printf("\nrotate_before_push goal: %d, dir: %d\n", goal, direction);
 	if (direction > 0)
 	{
 		while (stacks->a_stack->num != goal)
