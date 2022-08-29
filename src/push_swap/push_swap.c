4fd2cae01d803a57dc777a9fa5c248953a80bd16
Andre Backman <abackman@student.hive.fi>
2022-08-29 14:13:15 +0300

    sorts size 100 with 1000 moves

--- src/push_swap/push_swap.c
+++ src/push_swap/push_swap.c
@@ -6,7 +6,7 @@
 /*   By: abackman <abackman@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/06/10 16:56:19 by abackman          #+#    #+#             */
-/*   Updated: 2022/08/26 19:22:10 by abackman         ###   ########.fr       */
+/*   Updated: 2022/08/29 14:09:56 by abackman         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -44,7 +44,7 @@ int	sort_stacks(t_pusha *stacks)
 	else if (stacks->a_size <= 5)
 		return (sort_small(stacks));
 	else
-		return(push_chunks(stacks, 4));
+		return(push_chunks(stacks, 2));
 	return (0);
 }
 
