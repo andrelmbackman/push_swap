e3817ab8ddcb4e810ecacd4bd28efe5c394537f4
Andre Backman <abackman@student.hive.fi>
2022-08-31 20:17:50 +0300

    now pushes 2 chunks simultaneously

--- src/push_swap/push_chunks1.c
+++ src/push_swap/push_chunks1.c
@@ -6,7 +6,7 @@
 /*   By: abackman <abackman@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/08/25 15:05:05 by abackman          #+#    #+#             */
-/*   Updated: 2022/08/30 16:49:50 by abackman         ###   ########.fr       */
+/*   Updated: 2022/08/31 19:59:05 by abackman         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -95,13 +95,37 @@ static void	one_chunk(t_pusha *stacks, int size)
 	}
 } */
 
+static void	final_rotate(t_pusha *stacks)
+{
+	t_stack	*tmp;
+	int		i;
 
+	tmp = stacks->a_stack;
+	i = 0;
+	while (tmp->next != stacks->a_stack)
+	{
+		if (tmp->num == stacks->min)
+			break ;
+		tmp = tmp->next;
+		i++;
+	}
+	if (i < (stacks->a_size / 2))
+	{
+		while (!sorted(stacks))
+			exec_ra(stacks);
+	}
+	else
+	{
+		while (!sorted(stacks))
+			exec_rra(stacks);
+	}
+}
 /*
 ** This find_rotate is used with two_chunks, the functions used for one_chunk
 ** can be found above.
 */
 
-static int	find_rotate(t_pusha *stacks, int chunk)
+static int	find_rotate(t_pusha *stacks, int chunk, int max)
 {
 	t_stack	*front;
 	t_stack	*back;
@@ -110,8 +134,8 @@ static int	find_rotate(t_pusha *stacks, int chunk)
 	back = stacks->a_stack;
 	while (front->next != back && back->prev != front)
 	{
-		if (front->chunk == chunk || front->chunk == (5 - chunk) \
-		|| back->chunk == chunk || back->chunk == (5 - chunk))
+		if (front->chunk == chunk || front->chunk == (max - chunk) \
+		|| back->chunk == chunk || back->chunk == (max - chunk))
 			break ;
 		front = front->next;
 		back = back->prev;
@@ -119,9 +143,9 @@ static int	find_rotate(t_pusha *stacks, int chunk)
 			break ;
 	}
 	//ft_printf("\nfind_rotate\nchunk: %d frontchunk: %d backchunk: %d", chunk, front->chunk, back->chunk);
-	if (front->chunk == chunk || front->chunk == (5 - chunk))
+	if (front->chunk == chunk || front->chunk == (max - chunk))
 	{
-		if ((back->chunk == chunk || back->chunk == (5 - chunk)) && \
+		if ((back->chunk == chunk || back->chunk == (max - chunk)) && \
 		back->num < front->num)
 			return (rotate_before_push(stacks, back->num, -1));
 		else
@@ -131,7 +155,34 @@ static int	find_rotate(t_pusha *stacks, int chunk)
 		return (rotate_before_push(stacks, back->num, -1));
 }
 
-static void	two_chunks(t_pusha *stacks, int size, int count)
+/* static void	two_chunks(t_pusha *stacks, int size, int count, int max)
+{
+	int	i;
+	int	chunk;
+
+	i = -1;
+	chunk = max - count;
+	if (chunk == 2)
+		chunk *= 2;
+	while (++i < (size * 2))
+	{
+		if (stacks->a_stack == NULL)
+			break ;
+		//ft_printf("\ntwo_chunks\n");
+		find_rotate(stacks, chunk);
+		exec_pb(stacks);
+		if (stacks->b_stack->chunk != chunk)
+		{
+			if (stacks->a_stack->chunk != chunk && stacks->a_stack->chunk != \
+			(5 - chunk))
+				exec_rr(stacks);
+			else
+				exec_rb(stacks);
+		}
+	}
+} */
+
+/* static void	two_chunks(t_pusha *stacks, int size, int count, int max)
 {
 	int	i;
 
@@ -141,13 +192,65 @@ static void	two_chunks(t_pusha *stacks, int size, int count)
 		if (stacks->a_stack == NULL)
 			break ;
 		//ft_printf("\ntwo_chunks\n");
-		find_rotate(stacks, count);
+		find_rotate(stacks, count, max);
 		exec_pb(stacks);
-		if (stacks->b_stack->chunk == (5 - count))
+		if (stacks->b_stack->chunk == count)
 		{
 			if (stacks->a_stack->chunk != count && stacks->a_stack->chunk != \
-			(5 - count))
+			(max - count))
 				exec_rr(stacks);
+			else
+				exec_rb(stacks);
+		}
+	}
+} */
+
+static int	stacks_left(t_stack *a, int chunk1, int chunk2)
+{
+	t_stack	*tmp;
+	int		ret;
+
+	tmp = a;
+	ret = 0;
+	if (a == NULL)
+		return (0);
+	while (tmp->next != a)
+	{
+		if (tmp->chunk == chunk1 || tmp->chunk == chunk2)
+			ret = 1;
+		tmp = tmp->next;
+	}
+	if (tmp->chunk == chunk1 || tmp->chunk == chunk2)
+			ret = 1;
+	/* if (ret == 0)
+		ft_printf("\nno stacks left >> %d %d", chunk1, chunk2); */
+	return (ret);
+}
+
+static void	two_chunks(t_pusha *stacks, int size, int cur, int max)
+{
+	int	i;
+
+	i = 0;
+	if (size)
+		i = -1;
+	while (stacks_left(stacks->a_stack, cur, max - cur))
+	{
+		if (stacks->a_stack == NULL)
+			break ;
+		//ft_printf("\ntwo_chunks\n");
+		find_rotate(stacks, cur, max);
+		exec_pb(stacks);
+		if (stacks->b_stack->chunk == cur)
+		{
+			if (stacks->a_stack)
+			{
+				if (stacks->a_stack->chunk != cur && stacks->a_stack->chunk != \
+				(max - cur))
+					exec_rr(stacks);
+				else
+					exec_rb(stacks);
+			}
 			else
 				exec_rb(stacks);
 		}
@@ -161,26 +264,33 @@ static void	two_chunks(t_pusha *stacks, int size, int count)
 
 int	push_chunks(t_pusha *stacks, int count)
 {
-	int	i;
+	int	cur;
 	int	size;
 
-	i = 0;
+	cur = (count / 2) + 1;
 	size = stacks->a_size / count;
-	while (++i <= count)
+	if (count % 2 == 0)
+		count++;
+	//stacks->print = 0;
+	while (--cur >= 1)
 	{
-		print_stacks(stacks);
+		//print_stacks(stacks);
+		//ft_printf("\ncur: %d other: %d\n", cur, count - cur);
 		if (stacks->a_stack == NULL)
 			break ;
-		if (i == count)
+		if (cur == 1)
 			size = stacks->a_size;
 		//ft_printf("\nin push_chunks\ni: %d size: %d count: %d\n", i, size, count);
 		//one_chunk(stacks, size);
-		two_chunks(stacks, size, i);
+		two_chunks(stacks, size, cur, count);
 	}
 	//print_stacks(stacks);
+	while (stacks->a_stack != NULL)
+		exec_pb(stacks);
 	rotate_pushback(stacks);
-	while (!sorted(stacks))
-		exec_rra(stacks);
+	//print_stacks(stacks);
+	if (!sorted(stacks))
+		final_rotate(stacks);
 	//print_stacks(stacks);
 	return (0);
 }
