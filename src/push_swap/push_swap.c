1c63357afc8499a13510ae63d901bfe12dd5253b
Andre Backman <abackman@student.hive.fi>
2022-08-31 20:18:41 +0300

    splits larger sizes into 10 chunks, some cases could use 11..

--- src/push_swap/push_swap.c
+++ src/push_swap/push_swap.c
@@ -6,7 +6,7 @@
 /*   By: abackman <abackman@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/06/10 16:56:19 by abackman          #+#    #+#             */
-/*   Updated: 2022/08/30 14:58:17 by abackman         ###   ########.fr       */
+/*   Updated: 2022/08/31 19:27:52 by abackman         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -45,10 +45,10 @@ int	sort_stacks(t_pusha *stacks)
 		return (sort_small(stacks));
 	else
 	{
-		/* if (stacks->a_size <= 100)
-			return(push_chunks(stacks, 2));
-		else */
-			return (push_chunks(stacks, 4));
+		if (stacks->a_size <= 100)
+			return(push_chunks(stacks, 4));
+		else
+			return (push_chunks(stacks, 10));
 	}
 }
 
