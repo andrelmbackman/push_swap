682805cbfa88b7e7e60f9f6e84d4306222314a27
Andre home <andrebackmann@gmail.com>
2022-09-08 14:20:26 +0300

    removed unused sort_big.c, changed to sort_medium.c

--- src/push_swap/sort_medium.c
+++ src/push_swap/sort_medium.c
@@ -3,16 +3,16 @@
 /*                                                        :::      ::::::::   */
 /*   sort_big.c                                         :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
-/*   By: abackman <abackman@student.42.fr>          +#+  +:+       +#+        */
+/*   By: abackman <abackman@student.hive.fi>        +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/08/04 15:33:09 by abackman          #+#    #+#             */
-/*   Updated: 2022/08/18 13:07:13 by abackman         ###   ########.fr       */
+/*   Updated: 2022/09/08 14:18:26 by abackman         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "../../includes/push_swap.h"
 
-int	sort_big(t_pusha *stacks)
+int	sort_medium(t_pusha *stacks)
 {
 	if (stacks)
 		return (1);
