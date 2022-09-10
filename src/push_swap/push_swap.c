ed0419832d498074d8c0245a930ac020956f2078
Andre home <andrebackmann@gmail.com>
2022-09-10 16:58:48 +0300

    calling the right algorithm at the right sizes, complexities grow relative to the size

--- src/push_swap/push_swap.c
+++ src/push_swap/push_swap.c
@@ -6,7 +6,7 @@
 /*   By: abackman <abackman@student.hive.fi>        +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/06/10 16:56:19 by abackman          #+#    #+#             */
-/*   Updated: 2022/09/08 21:16:11 by abackman         ###   ########.fr       */
+/*   Updated: 2022/09/10 16:52:03 by abackman         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -39,17 +39,20 @@ int	sort_stacks(t_pusha *stacks)
 	if (stacks->a_size <= 1 || stacks->a_stack == NULL)
 		return (-1);
 	stacks->print = 1;
-	if (stacks->a_size > 100)
+	if (stacks->a_size > 119)
 		stacks->chunk_no = 32;
-	else
+	else if (stacks->a_size > 23)
 		stacks->chunk_no = 12;
+	else
+		stacks->chunk_no = 6;
 	stacks->chunk_top = (stacks->chunk_no / 2) + 2;
 	stacks->chunk_bot = (stacks->chunk_no / 2) - 1;
+	get_destined(stacks, stacks->min, stacks->max, stacks->a_size);
 	if (sorted(stacks))
 		return (1);
 	else if (stacks->a_size <= 5)
 		return (sort_small(stacks));
-	else if (stacks->a_size <= 20)
+	else if (stacks->a_size < 24)
 		return (sort_medium(stacks));
 	else
 		return(sort_big(stacks, stacks->chunk_no));
@@ -66,7 +69,39 @@ int	main(int ac, char **av)
 		return (free_stacks(stacks, ERROR));
 	if (check_ints(stacks, ac, av) == -1)
 		return (-1);
-	get_destined(stacks->a_stack, stacks->min, stacks->max, stacks->a_size);
 	sort_stacks(stacks);
 	return (free_stacks(stacks, NOMESSAGE));
 }
+
+
+/* #include <fcntl.h>
+
+int	main(void)
+{
+	t_pusha	*stacks;
+	char	*buf;
+	char	*buf1[2];
+	int 	fp = open("7_permutations.txt", O_RDONLY);
+
+	if (fp < 0)
+		return (-1);
+	buf1[0] = NULL;
+	while (get_next_line(fp, &buf))
+	{
+		stacks = init_stacks();
+		//ft_printf("\n%s\n", buf);
+		if (stacks == NULL)
+			return (free_stacks(stacks, ERROR));
+		buf1[1] = buf;
+		if (check_ints(stacks, 2, buf1) == -1)
+			return (-1);
+		stacks->chunk_no = 6;
+		get_destined(stacks, stacks->min, stacks->max, stacks->a_size);
+		buf = strjoin_pro("./checker ", buf, 2);
+		system((const char *)buf);
+		sort_stacks(stacks);
+		free_stacks(stacks, NOMESSAGE);
+		ft_strdel(&buf);
+	}
+	return (0);
+} */
