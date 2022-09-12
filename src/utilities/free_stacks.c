664dba4ec72726068b0c674e4ea0b3058a88328d
Andre Backman <abackman@student.hive.fi>
2022-09-12 20:17:48 +0300

    cleaned up code

--- src/utilities/free_stacks.c
+++ src/utilities/free_stacks.c
@@ -6,7 +6,7 @@
 /*   By: abackman <abackman@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/08/01 18:41:10 by abackman          #+#    #+#             */
-/*   Updated: 2022/08/31 12:25:06 by abackman         ###   ########.fr       */
+/*   Updated: 2022/09/12 14:45:04 by abackman         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -100,13 +100,12 @@ static int	del_stack(t_stack *stack)
 
 int	free_stacks(t_pusha *stacks, int status)
 {
-	/* if (!stacks->print)
-		print_stacks(stacks); */
+	//if (!stacks->print)
+	//	print_stacks(stacks);
 	if (stacks->a_size && stacks->a_stack != NULL)
 		del_stack(stacks->a_stack);
 	if (stacks->b_size && stacks->b_stack != NULL)
 		del_stack(stacks->b_stack);
-	//ft_printf("\nFreeing staxx\n");
 	if (stacks)
 	{
 		free(stacks);
