249648b09695633a7849bf7c03853cde2e2d3c90
Andre Backman <abackman@student.hive.fi>
2022-09-12 20:19:14 +0300

    new version saves all moves before executing, seg fault at empty input + ctrl D, or faulty input

--- src/checker/checker.c
+++ src/checker/checker.c
@@ -6,20 +6,77 @@
 /*   By: abackman <abackman@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/05/31 18:07:57 by abackman          #+#    #+#             */
-/*   Updated: 2022/09/12 15:36:58 by abackman         ###   ########.fr       */
+/*   Updated: 2022/09/12 20:14:44 by abackman         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "../../includes/push_swap.h"
 
-static int	exec_move(t_pusha *stacks, int status)
+static int	exec_move(t_pusha *stacks, t_move *head)
+{
+	t_move *tmp;
+
+	tmp = head;
+	//ft_printf("\nexec_move\n");
+	if (head == NULL)
+		return (0);
+	print_stacks(stacks);
+	while (tmp->next != NULL)
+	{
+		g_pushtable[tmp->index](stacks);
+		//ft_printf("node->command: %s index: %d\n", tmp->str, tmp->index);
+		tmp = tmp->next;
+		print_stacks(stacks);
+	}
+	g_pushtable[tmp->index](stacks);
+	print_stacks(stacks);
+	//ft_printf("node->command: %s index: %d\n", tmp->str, tmp->index);
+	if (stacks == NULL || head == NULL)
+		return (-1);
+	return (1);
+}
+
+
+/* static int	exec_move(t_pusha *stacks, int status)
 {
 	if (stacks == NULL || status == -1)
 		return (-1);
 	return (g_pushtable[status](stacks));
+} */
+
+static int	valid_move(t_move *head, char **valid)
+{
+	int		i;
+	int		ret;
+	t_move	*tmp;
+
+	i = 0;
+	ret = 0;
+	tmp = head;
+	if (head == NULL)
+		return (0);
+	while (tmp != NULL)
+	{
+		ret = 0;
+		i = 0;
+		while (valid[i] != NULL)
+		{
+			if (!ft_strcmp((const char *)valid[i], (const char *)tmp->str))
+			{
+				ret = 1;
+				tmp->index = i;
+				//ft_printf("\nvalid_move\n");
+			}
+			i++;
+		}
+		if (!ret)
+			return (0);
+		tmp = tmp->next;
+	}
+	return (1);
 }
 
-static int	valid_move(char *buf, char **valid)
+/* static int	valid_move(char *buf, char **valid)
 {
 	int	i;
 
@@ -31,14 +88,70 @@ static int	valid_move(char *buf, char **valid)
 		i++;
 	}
 	return (-1);
+} */
+
+static int	add_move(t_move **head, char *buf)
+{
+	t_move	*new;
+	t_move	*tmp;
+
+	tmp = NULL;
+	new = (t_move *)malloc(sizeof(t_move));
+	if (!new)
+		return (-1);
+	new->next = NULL;
+	new->str = ft_strdup((const char *)buf);
+	new->index = -1;
+	if (*head == NULL)
+	{
+		*head = new;
+		//ft_printf("\nadd_move; new->str: %s\n", new->str);
+		return (1);
+	}
+	tmp = *head;
+	while (tmp->next != NULL)
+		tmp = tmp->next;
+	tmp->next = new;
+	//ft_printf("\nadd_move; new->str: %s\n", new->str);
+	return (1);
 }
 
 static int	moves_check(t_pusha *stacks)
 {
 	int		status;
 	char	*buf;
+	t_move	*head;
 
 	status = 0;
+	head = NULL;
+	while (get_next_line((const int)STDIN_FILENO, &buf))
+	{
+		if (add_move(&head, buf) < 0)
+		{
+			ft_strdel(&buf);
+			return (free_all(stacks, &head, ERROR));
+		}
+		ft_strdel(&buf);
+	}
+	//ft_strdel(&buf);
+	if (!valid_move(head, stacks->valid_moves))
+		return (free_all(stacks, &head, ERROR));
+	else
+		exec_move(stacks, head);
+	//ft_printf("\nmoves_check after looop\n");
+	free_moves(&head);
+	//print_stacks(stacks);
+	return (check_stacks(stacks));
+}
+
+/* static int	moves_check(t_pusha *stacks)
+{
+	int		status;
+	char	*buf;
+	t_move	*head;
+
+	status = 0;
+	head = NULL;
 	while (1)
 	{
 		if (!get_next_line((const int)STDIN_FILENO, &buf))
@@ -56,6 +169,7 @@ static int	moves_check(t_pusha *stacks)
 	ft_strdel(&buf);
 	return (check_stacks(stacks));
 }
+ */
 
 int	main(int ac, char **av)
 {
@@ -68,10 +182,10 @@ int	main(int ac, char **av)
 		return (1);
 	stacks = init_stacks();
 	if (stacks == NULL)
-		return (return_status(-1));
+		return (return_status(ERROR));
 	moves = ft_strsplit("sa sb ss pa pb ra rb rr rra rrb rrr", (char)32);
 	if (moves == NULL)
-		return (return_status(-1));
+		return (return_status(ERROR));
 	stacks->valid_moves = moves;
 	if (check_ints(stacks, ac, av) == -1)
 	{
