a9c9c5e739a17020673fc0b3a52b345669edb8e6
Andre Backman <abackman@student.hive.fi>
2022-09-14 18:49:41 +0300

    removed extra comments, formatted according to norminette

--- src/push_swap/push_swap.c
+++ src/push_swap/push_swap.c
@@ -6,7 +6,7 @@
 /*   By: abackman <abackman@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/06/10 16:56:19 by abackman          #+#    #+#             */
-/*   Updated: 2022/09/13 10:36:27 by abackman         ###   ########.fr       */
+/*   Updated: 2022/09/14 18:49:07 by abackman         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -31,7 +31,7 @@ static int	sort_stacks(t_pusha *stacks)
 	else if (stacks->a_size < 24)
 		return (sort_medium(stacks));
 	else
-		return(sort_big(stacks, stacks->chunk_no));
+		return (sort_big(stacks, stacks->chunk_no));
 }
 
 int	main(int ac, char **av)
@@ -45,39 +45,6 @@ int	main(int ac, char **av)
 		return (free_stacks(stacks, ERROR));
 	if (check_ints(stacks, ac, av) == -1)
 		return (-1);
-	//print_stacks(stacks);
 	sort_stacks(stacks);
 	return (free_stacks(stacks, NOMESSAGE));
 }
-
-/* #include <fcntl.h>
-
-int	main(void)
-{
-	t_pusha	*stacks;
-	char	*buf;
-	char	*buf1[2];
-	int 	fp = open("7_permutations.txt", O_RDONLY);
-
-	if (fp < 0)
-		return (-1);
-	buf1[0] = NULL;
-	while (get_next_line(fp, &buf))
-	{
-		stacks = init_stacks();
-		//ft_printf("\n%s\n", buf);
-		if (stacks == NULL)
-			return (free_stacks(stacks, ERROR));
-		buf1[1] = buf;
-		if (check_ints(stacks, 2, buf1) == -1)
-			return (-1);
-		stacks->chunk_no = 6;
-		get_destined(stacks, stacks->min, stacks->max, stacks->a_size);
-		buf = strjoin_pro("./checker ", buf, 2);
-		system((const char *)buf);
-		sort_stacks(stacks);
-		free_stacks(stacks, NOMESSAGE);
-		ft_strdel(&buf);
-	}
-	return (0);
-} */
