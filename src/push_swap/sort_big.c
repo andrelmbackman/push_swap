895abd641ed85c4e4f746e39ae0f0ac486976c0d
Andre Backman <abackman@student.hive.fi>
2022-09-15 14:16:40 +0300

    formatted properly, added comments

--- src/push_swap/sort_big.c
+++ src/push_swap/sort_big.c
@@ -6,11 +6,17 @@
 /*   By: abackman <abackman@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/08/25 15:05:05 by abackman          #+#    #+#             */
-/*   Updated: 2022/09/13 13:07:18 by abackman         ###   ########.fr       */
+/*   Updated: 2022/09/15 14:16:18 by abackman         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "../../includes/push_swap.h"
+
+/*
+** After the stacks are in order, the smallest number needs to be at the top.
+** final_rotate finds out which rotation would require fewer moves to achieve
+** this and then rotates until the stack is sorted in ascending order.
+*/
 
 static void	final_rotate(t_pusha *stacks)
 {
@@ -26,7 +32,7 @@ static void	final_rotate(t_pusha *stacks)
 		tmp = tmp->next;
 		i++;
 	}
-	if (i < (stacks->a_size / 2))
+	if (i <= (stacks->a_size / 2))
 	{
 		while (!sorted(stacks))
 			exec_ra(stacks);
@@ -38,126 +44,9 @@ static void	final_rotate(t_pusha *stacks)
 	}
 }
 
-/* static int	find_rotate(t_pusha *stacks, int chunk, int max)
-{
-	t_stack	*front;
-	t_stack	*back;
-
-	front = stacks->a_stack;
-	back = stacks->a_stack;
-	while (front->next != back && back->prev != front)
-	{
-		if (front->chunk == chunk || front->chunk == (max - chunk) \
-		|| back->chunk == chunk || back->chunk == (max - chunk))
-			break ;
-		front = front->next;
-		back = back->prev;
-		if (front == back)
-			break ;
-	}
-	//ft_printf("\nfind_rotate\nchunk: %d frontchunk: %d backchunk: %d", chunk, front->chunk, back->chunk);
-	if (front->chunk == chunk || front->chunk == (max - chunk))
-	{
-		if ((back->chunk == chunk || back->chunk == (max - chunk)) && \
-		back->num < front->num)
-			return (rotate_before_push(stacks, back->num, -1));
-		else
-			return (rotate_before_push(stacks, front->num, 1));
-	}
-	else
-		return (rotate_before_push(stacks, back->num, -1));
-} */
-
-
-/* static int	stacks_left(t_stack *a, int chunk1, int chunk2)
-{
-	t_stack	*tmp;
-	int		ret;
-
-	tmp = a;
-	ret = 0;
-	if (a == NULL)
-		return (0);
-	while (tmp->next != a)
-	{
-		if (tmp->chunk == chunk1 || tmp->chunk == chunk2)
-			ret = 1;
-		tmp = tmp->next;
-	}
-	if (tmp->chunk == chunk1 || tmp->chunk == chunk2)
-			ret = 1;
-	if (ret == 0)
-		ft_printf("\nno stacks left >> %d %d", chunk1, chunk2);
-	return (ret);
-}
- */
-/* static void	two_chunks(t_pusha *stacks, int size, int cur, int max)
-{
-	int	i;
-
-	i = 0;
-	if (size)
-		i = -1;
-	while (stacks_left(stacks->a_stack, cur, max - cur))
-	{
-		if (stacks->a_stack == NULL)
-			break ;
-		//ft_printf("\ntwo_chunks\n");
-		find_rotate(stacks, cur, max);
-		exec_pb(stacks);
-		if (stacks->b_stack->chunk == cur)
-		{
-			if (stacks->a_stack)
-			{ 
-				if (stacks->a_stack->chunk != cur && stacks->a_stack->chunk != \
-				(max - cur))
-					exec_rr(stacks);
-				else
-					exec_rb(stacks);
-			}
-			else
-				exec_rb(stacks);
-		}
-	}
-} */
-
 /*
-** push_chunks finds the optimal moves to push chunks of similar size to
-** b_stack, the count argument lets you specify the desired number of chunks.
+** Counts how many nodes are within the chunk range chunk_bot - chunk_top.
 */
-/* 
-int	push_chunks(t_pusha *stacks, int count)
-{
-	int	cur;
-	int	size;
-
-	cur = (count / 2) + 1;
-	size = stacks->a_size / count;
-	if (count % 2 == 0)
-		count++;
-	stacks->print = 0;
-	while (--cur >= 1)
-	{
-		//print_stacks(stacks);
-		ft_printf("\ncur: %d other: %d\n", cur, count - cur);
-		if (stacks->a_stack == NULL)
-			break ;
-		if (cur == 1)
-			size = stacks->a_size;
-		//ft_printf("\nin push_chunks\ni: %d size: %d count: %d\n", i, size, count);
-		//one_chunk(stacks, size);
-		two_chunks(stacks, size, cur, count);
-	}
-	//print_stacks(stacks);
-	while (stacks->a_stack != NULL)
-		exec_pb(stacks);
-	rotate_pushback(stacks);
-	//print_stacks(stacks);
-	if (!sorted(stacks))
-		final_rotate(stacks);
-	//print_stacks(stacks);
-	return (0);
-} */
 
 static int	size_4_chunks(t_pusha *stacks)
 {
@@ -178,6 +67,12 @@ static int	size_4_chunks(t_pusha *stacks)
 		ret++;
 	return (ret);
 }
+
+/*
+** Checks whether a double move (rr) would be beneficial. For this algorithm
+** to be efficient we need to push half of the chunks to the bottom, that way
+** fewer moves are required when looking to push back certain numbers to a.
+*/
 
 static void	check_rotate(t_stack *a, t_stack *b, t_pusha *s)
 {
@@ -201,8 +96,13 @@ static void	check_rotate(t_stack *a, t_stack *b, t_pusha *s)
 			exec_ra(s);
 	}
 	else if (b->chunk == (s->chunk_bot + 1) || b->chunk == (s->chunk_top - 1))
-			exec_rb(s);
+		exec_rb(s);
 }
+
+/*
+** Given the size of how many nodes should be pushed to b_stack, rotates
+** and pushes nodes to the b_stack.
+*/
 
 static void	push_4_chunks(t_pusha *stacks, int size, int min, int max)
 {
@@ -220,6 +120,12 @@ static void	push_4_chunks(t_pusha *stacks, int size, int min, int max)
 	}
 }
 
+/*
+** Sorts stacks that are 24 numbers or larger. Pushes 4 chunks at a time to
+** b_stack, giving them an even spread but still not requiring a lot of moves.
+** Sorts them back in order by finding either the smallest or largest number.
+*/
+
 int	sort_big(t_pusha *stacks, int count)
 {
 	int	size;
@@ -231,7 +137,6 @@ int	sort_big(t_pusha *stacks, int count)
 		stacks->chunk_bot -= 2;
 		stacks->chunk_top += 2;
 		size = size_4_chunks(stacks);
-		//print_stacks(stacks);
 	}
 	rotate_pushback(stacks);
 	final_rotate(stacks);
