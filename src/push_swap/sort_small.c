c9d3d3b0dfe19e63fd18293f12b438289db7924a
Andre Backman <abackman@student.hive.fi>
2022-09-15 15:09:23 +0300

    cleaned up and added comments

--- src/push_swap/sort_small.c
+++ src/push_swap/sort_small.c
@@ -3,14 +3,18 @@
 /*                                                        :::      ::::::::   */
 /*   sort_small.c                                       :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
-/*   By: abackman <abackman@student.hive.fi>        +#+  +:+       +#+        */
+/*   By: abackman <abackman@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/08/04 12:20:29 by abackman          #+#    #+#             */
-/*   Updated: 2022/08/17 15:13:45 by abackman         ###   ########.fr       */
+/*   Updated: 2022/09/15 15:08:43 by abackman         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "../../includes/push_swap.h"
+
+/*
+** If the stack is not in order, swaps the two nodes to sort them.
+*/
 
 static int	sort_two(t_pusha *stacks)
 {
@@ -19,28 +23,32 @@ static int	sort_two(t_pusha *stacks)
 	return (0);
 }
 
+/*
+** Finds the moves to sort a stack of size three, depending on the order.
+*/
+
 int	sort_three(t_pusha *stacks)
 {
 	t_stack	*a;
 
 	a = stacks->a_stack;
-	if (a->num > a->next->num && a->next->num < a->prev->num &&\
-	 a->prev->num > a->num)
+	if (a->num > a->next->num && a->next->num < a->prev->num && \
+	a->prev->num > a->num)
 		return (exec_sa(stacks));
-	else if (a->num > a->next->num && a->next->num < a->prev->num &&\
-	 a->prev->num < a->num)
+	else if (a->num > a->next->num && a->next->num < a->prev->num && \
+	a->prev->num < a->num)
 		return (exec_ra(stacks));
-	else if (a->num < a->next->num && a->next->num > a->prev->num &&\
-	 a->prev->num < a->num)
+	else if (a->num < a->next->num && a->next->num > a->prev->num && \
+	a->prev->num < a->num)
 		return (exec_rra(stacks));
-	else if (a->num < a->next->num && a->next->num > a->prev->num &&\
-	 a->prev->num > a->num)
+	else if (a->num < a->next->num && a->next->num > a->prev->num && \
+	a->prev->num > a->num)
 	{
 		exec_sa(stacks);
 		return (exec_ra(stacks));
 	}
-	else if (a->num > a->next->num && a->next->num > a->prev->num &&\
-	 a->prev->num < a->num)
+	else if (a->num > a->next->num && a->next->num > a->prev->num && \
+	a->prev->num < a->num)
 	{
 		exec_sa(stacks);
 		return (exec_rra(stacks));
@@ -48,12 +56,17 @@ int	sort_three(t_pusha *stacks)
 	return (0);
 }
 
+/*
+** Rotates until the smallest number is at the top, and if the stack is not
+** sorted this number is pushed to b_stack. The three remaining numbers are
+** sorted and then the smallest is pushed back from b to the top of a.
+*/
+
 static int	sort_four(t_pusha *stacks)
 {
-	t_stack *a;
+	t_stack	*a;
 
 	a = stacks->a_stack;
-	//ft_printf("\nsort_four\na_stack: %d\nmin: %d\n", a->num, stacks->min);
 	if (a->next->num == stacks->min || a->next->next->num == stacks->min)
 	{
 		while (stacks->a_stack->num != stacks->min)
@@ -67,6 +80,10 @@ static int	sort_four(t_pusha *stacks)
 	sort_three(stacks);
 	return (exec_pa(stacks));
 }
+
+/*
+** Decides which function to call depending on stack size.
+*/
 
 int	sort_small(t_pusha *stacks)
 {
