2dcd795835743deb26289de0dcffef0c12fa1dfc
Andre Backman <abackman@student.hive.fi>
2022-09-15 15:32:01 +0300

    cleaned up and added comments

--- src/utilities/moves1_swap.c
+++ src/utilities/moves1_swap.c
@@ -6,67 +6,16 @@
 /*   By: abackman <abackman@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/06/10 15:43:00 by abackman          #+#    #+#             */
-/*   Updated: 2022/09/13 19:35:06 by abackman         ###   ########.fr       */
+/*   Updated: 2022/09/15 15:31:37 by abackman         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "../../includes/push_swap.h"
 
-/* int	exec_sa(t_pusha *stacks)
-{
-	t_stack	*tmp;
-
-	if (stacks->a_size < 2)
-		return (0);
-	tmp = stacks->a_stack;
-	stacks->a_stack = stacks->a_stack->next;
-	if (stacks->a_size > 2)
-	{
-		stacks->a_stack->prev = tmp->prev;
-		stacks->a_stack->prev->next = stacks->a_stack;
-		tmp->next = stacks->a_stack->next;
-	}
-	else
-	{
-		stacks->a_stack->prev = tmp;
-		stacks->a_stack->prev->next = stacks->a_stack;
-	}
-	tmp->prev = stacks->a_stack;
-	stacks->a_stack->next = tmp;
-	if (stacks->print == 1)
-		write(1, "sa\n", 3);
-	if (stacks->v == 1)
-		print_stacks(stacks);
-	return (1);
-} */
-
-/* int	exec_sb(t_pusha *stacks)
-{
-	t_stack	*tmp;
-
-	if (stacks->b_size < 2)
-		return (0);
-	tmp = stacks->b_stack;
-	stacks->b_stack = stacks->b_stack->next;
-	if (stacks->b_size > 2)
-	{
-		stacks->b_stack->prev = tmp->prev;
-		stacks->b_stack->prev->next = stacks->b_stack;
-		tmp->next = stacks->b_stack->next;
-	}
-	else
-	{
-		stacks->b_stack->prev = tmp;
-		stacks->b_stack->prev->next = stacks->b_stack;
-	}
-	tmp->prev = stacks->b_stack;
-	stacks->b_stack->next = tmp;
-	if (stacks->print == 1)
-		write(1, "sb\n", 3);
-	if (stacks->v == 1)
-		print_stacks(stacks);
-	return (1);
-} */
+/*
+** Swaps the first two nodes of the a_stack. If called by push_swap and print
+** is set to one it will write the command to stdout.
+*/
 
 int	exec_sa(t_pusha *stacks)
 {
@@ -87,6 +36,11 @@ int	exec_sa(t_pusha *stacks)
 	return (1);
 }
 
+/*
+** Swaps the first two nodes of the b_stack. If called by push_swap and print
+** is set to one it will write the command to stdout.
+*/
+
 int	exec_sb(t_pusha *stacks)
 {
 	int	tmp;
@@ -106,6 +60,12 @@ int	exec_sb(t_pusha *stacks)
 	return (1);
 }
 
+/*
+** Swaps the first two nodes of both a_stack and b_stack.
+** If called by push_swap and print is set to one, it is increased so that
+** exec_sa and exec_sb do not write out their operations to stdout.
+*/
+
 int	exec_ss(t_pusha *stacks)
 {
 	if (stacks->print)
@@ -114,15 +74,7 @@ int	exec_ss(t_pusha *stacks)
 		stacks->v = 2;
 	exec_sa(stacks);
 	exec_sb(stacks);
-	/* if (!exec_sa(stacks) && !exec_sb(stacks))
-	{
-		if (stacks->print == 2)
-			stacks->print = 1;
-		if (stacks->v == 2)
-			stacks->v = 1;
-		return (0);
-	} 
-	else*/ if (stacks->print == 2)
+	if (stacks->print == 2)
 	{
 		write(1, "ss\n", 3);
 		stacks->print = 1;
