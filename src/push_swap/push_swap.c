06e2fc81da2c310d3c1c108bc78b24ea8ffa4eb9
Andre Backman <abackman@student.hive.fi>
2022-09-16 13:19:47 +0300

    added function to print usage message when used with -h flag

--- src/push_swap/push_swap.c
+++ src/push_swap/push_swap.c
@@ -6,11 +6,29 @@
 /*   By: abackman <abackman@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/06/10 16:56:19 by abackman          #+#    #+#             */
-/*   Updated: 2022/09/15 14:03:41 by abackman         ###   ########.fr       */
+/*   Updated: 2022/09/16 13:15:53 by abackman         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "../../includes/push_swap.h"
+
+/*
+** Displays a usage message for the push_swap program.
+*/
+
+static int	push_swap_help(void)
+{
+	ft_printf("\n{green}usage: ./push_swap [integers to be sorted]{eoc}\n\n");
+	ft_printf("The integers can either be separate arguments or a string.\n");
+	ft_printf("push_swap will then check if the arguments are valid.\n");
+	ft_printf("If so, instructions (operations) to sort the integers\n");
+	ft_printf("will be printed. These are all valid instructions:");
+	ft_printf("\n{yellow}sa sb ss pa pb ra rb rr rra rrb rrr{eoc}\n");
+	ft_printf("To test it we can pipe the output of\n{purple}push_swap ");
+	ft_printf("{eoc}to the input of {cyan}checker{eoc}:{green}\n");
+	ft_printf("ARGS=\"4 5 1 2 3\"; ./push_swap $ARGS | ./checker $ARGS{eoc}\n");
+	return (0);
+}
 
 /*
 ** Assigns the appropriate number of chunks to divide into (where applicable),
@@ -43,8 +61,11 @@ int	main(int ac, char **av)
 {
 	t_pusha	*stacks;
 
+	stacks = NULL;
 	if (ac < 2 || !av[1][0])
 		return (1);
+	if (!ft_strcmp((const char *)av[1], (const char *)"-h"))
+		return (push_swap_help());
 	stacks = init_stacks();
 	if (stacks == NULL)
 		return (free_stacks(stacks, ERROR));
